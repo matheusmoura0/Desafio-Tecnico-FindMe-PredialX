0600b56dfedf14ed1ca348901a05ddb31f145f5e
Matheus <matheusmoura2110@gmail.com>
2022-08-19 11:17:10 -0300

    feat: add crud verifications

--- app/server/src/api/controllers/clienteController.js
+++ app/server/src/api/controllers/clienteController.js
@@ -2,6 +2,11 @@ const clienteService = require ('../services/clienteService');
 
 const create = async (req, res) => { 
     const{ name } = req.body;
+    if (!name) { 
+        return res.status(400).json(
+            { message : 'O Nome do cliente é obrigatório'}
+            );
+    }
     const cliente = await clienteService.create(name);
 
     return res.status(201).json(cliente);
@@ -16,26 +21,43 @@ const findAll = async (_req, res) => {
 const getById =  async (req, res) => { 
     const{ id } = req.params;
     const cliente = await  clienteService.getById(id);
+    if (!cliente) { 
+        return res.status(404).json(
+            { message : 'Cliente não encontrado'}
+            );
+    }
 
     return res.status(200).json(cliente);
 }
 
 const update = async  (req, res) => { 
-    const{ id } = req.params;
-    const{ name } = req.body;
-
-    const cliente =  await clienteService.update(id, name);
-
-    return res.status(200).json(cliente);
+    try {
+        const{ id } = req.params;
+        const{ name } = req.body;
+    
+        const cliente =  await clienteService.update(id, name);
+    
+        return res.status(200).json(cliente);
+    } catch (error) { 
+        return res.status(500).json(
+            { message : 'Erro ao atualizar cliente'}
+            );
+    }
 }
 
 const deletebyId = async (req, res) => { 
-    const{ id } = req.params;
-    await clienteService.deletebyId(id);
-
-    return res.status(200).json(
-        { message : 'Cliente deletado com sucesso'}
-    );
+    try {
+        const{ id } = req.params;
+        await clienteService.deletebyId(id);
+        
+        return res.status(200).json(
+            { message : 'Cliente deletado com sucesso'}
+            );
+    } catch (error) { 
+        return res.status(500).json(
+            { message : 'Erro ao deletar cliente'}
+            );
+    }
 }
 
 module.exports = { 
